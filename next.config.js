b64d3a42be10e155aa3fe6930ff68aee154662b1
Long Vu <long.vu@bmbsoft.com.vn>
2023-06-06 14:36:38 +0700

    add dockerfile

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  experimental: {
+    outputStandalone: true,
+  },
 }
-
-module.exports = nextConfig
